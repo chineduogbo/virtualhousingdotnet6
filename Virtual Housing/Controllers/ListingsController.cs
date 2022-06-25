81c5eea133c1952b077710ee4b03f05a1141c56c
chineduogbo <chinedu.ogbo@lloydant.com>
2022-06-25 08:16:51 +0100

    added new

--- Virtual Housing/Controllers/ListingsController.cs
+++ Virtual Housing/Controllers/ListingsController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Virtual_Housing.Models.ViewModel;
+using Virtual_Housing.Services.Interfaces;
 
 namespace Virtual_Housing.Controllers
 {
     public class ListingsController : Controller
     {
-        public IActionResult Index()
+        private readonly IListingService _listingService;
+        public ListingsController(IListingService listingService)
         {
-            return View();
+            _listingService = listingService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ListingViewModel model = new();
+            model.Listings = await _listingService.ViewListings();
+            return View(model);
+        }
+        public IActionResult CreateListing()
+        {
+            ListingViewModel model = new();
+            return View(model);
         }
     }
 }
---
+++ Virtual Housing/Models/ViewModel/ListingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Virtual_Housing.Models.Dto;
+using Virtual_Housing.Models.Model;
+
+namespace Virtual_Housing.Models.ViewModel
+{
+    public class ListingViewModel
+    {
+        public CreateListingDto createListingDto { get; set; }
+        public List<Listings> Listings { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
